e1545b92ad8b3f2831918620ea94cd959152ff8c
kamilsen <sendej@gmail.com>
2021-05-19 10:17:16 +0200

    dodanie nowych danych kontaktowych

--- portfolio/src/components/contact/contact.js
+++ portfolio/src/components/contact/contact.js
@@ -2,14 +2,25 @@ import React from 'react'
 
 import styles from './contact.module.scss'
 
-import envelope from '../../assets/img/envelope4.png'
+import envelope from '../../assets/img/envelope5.png'
 
 const contact = () => {
     return(
         <section  name="contact" id="contact" className={styles.contact}>
-            <a href="mailto:kontakt@kamilsendecki.pl" className={styles.contactLink} target="_blank" rel="noopener">
                 <img src={envelope} alt="email" className={styles.envelope}/>
-            </a>
+                <p className={styles.name}>
+                        Kamil Sendecki
+                </p>
+                <p className={styles.mail}>
+                    <a href="mailto:kontakt@kamilsendecki.pl" target="_blank" rel="noopener noreferrer">
+                        kontakt@kamilsendecki.pl
+                    </a>
+                </p>
+                <p className={styles.phone}>
+                    <a  href="tel:48794144373" target="_blank" rel="noopener noreferrer">
+                        +48 794 144 373
+                    </a>
+                </p>
         </section>
     )
 }
